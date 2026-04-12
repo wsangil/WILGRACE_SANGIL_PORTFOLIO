8c13161959ce53c260ee41e8923c49494f6d34ea
wsangil <w.sangil.business@gmail.com>
2026-04-12 22:38:31 +0800

    Add new project images and update BOOKITBUDDY project details in script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 async function loadProjects() {
     // Hardcode the data so it works on GitHub Pages without a backend
     const projects = [
-        { name: "BOOKITBUDDY", tech: "Python, Database", image: "images/bookitbuddy.PNG" },
+        { name: "BOOKITBUDDY", 
+        image: ["images/bib.png", "images/bib2.png", "images/bib3.png", "images/bib4.png", "images/bib5.png", "images/bib6.png", "images/bib7.png", "images/bib8.png", "images/bib9.png"],
+        description: "A BulSU E-Library Reservation System. Developed using Python and a database, this project allows users to reserve function halls, manage their reservations, and access library resources efficiently."
+        },
         { name: "SOLVEIT", tech: "java, numerical methods", image: "images/solveit.png" },
         { name: "PEENOISE RACE", tech: "html, css, js", image: "images/peenoise.png" }
     ];
