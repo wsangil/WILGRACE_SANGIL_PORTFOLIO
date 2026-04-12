1b3500e88e2d3776a1f69d2dadd0771bff3beec3
wsangil <w.sangil.business@gmail.com>
2026-04-12 23:14:35 +0800

    Add new images for SOLVEIT project and update project details in script.js

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ async function loadProjects() {
         image: ["images/bib.png", "images/bib2.png", "images/bib3.png", "images/bib4.png", "images/bib5.png", "images/bib6.png", "images/bib7.png", "images/bib8.png", "images/bib9.png"],
         description: "A BulSU E-Library Reservation System. Developed using Python and a database, this project allows users to reserve function halls, manage their reservations, and access library resources efficiently."
         },
-        { name: "SOLVEIT", tech: "java, numerical methods", image: "images/solveit.png" },
+        { name: "SOLVEIT", 
+            image:["images/solveit.png", "images/solveit2.png", "images/solveit3.png", "images/solveit4.png", "images/solveit5.png", "images/solveit6.png"],
+            description: "A problem-solving application for numerical methods and algorithms developed using java language"},
         { name: "PEENOISE RACE", tech: "html, css, js", image: "images/peenoise.png" }
     ];
 
